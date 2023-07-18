60e83ad9317d0ce110b28497437d4a5558db4edb
Anukul <anukul@github.com>
2023-07-18 09:10:40 +0545

    first update
    added new file python_tut.py

---
+++ python_tut1.py
@@ -0,0 +1,16 @@
+#printing the hello world
+print("Hello world")
+
+#variables and operator in python
+a=6
+b=89
+c="hari"
+d=4+6j
+e=3.1415
+
+#type() returns the type of  variable
+print(a+b)
+print(type(a+b))
+print(type(c))
+print(type(d))
+print(type(e))
