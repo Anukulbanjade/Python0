65a2f92d7fb54506b92a48da0af9695fac8f95e5
Anukul <anukul@github.com>
2023-07-18 10:17:44 +0545

    updated at 2020/7/18 10:17 AM

--- comparision_operators.py
+++ comparision_operators.py
@@ -1,5 +1,5 @@
 '''
-Comparision operator 
+Boolean and Comparision operator 
 1. = =    "is equals to"
 2. ! =    "IS NOT EQUALS TO"
 3. <      "LESS THAN"
@@ -26,3 +26,26 @@ print(not(new1))
 
 print((new1 and new2)or (new2 and new3))
 
+#some useful functions
+#round()
+round1=3.141568925
+print(round(round1))
+print(round(round1,3)) #gives roundoff 3 digits
+
+
+
+#divmod()
+divmod1=divmod(15,6)
+print(divmod1)  #returns quotient and remainder 
+
+print(type(divmod1))     #gives result in tuple
+#Acessing the value
+print(divmod1[0])
+print(divmod1[1])
+
+#isinstance()
+print(isinstance(6,int))    #isinstance() check the value is specific data type or not here in this line of code it is checking 6 is int or not and returns it in bool format
+
+
+#pow(x,y,z)  x raise to y and remainder z
+print(pow(2,4,4))
