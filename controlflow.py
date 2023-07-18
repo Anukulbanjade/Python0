6abd9c1d170e3ea1dd6a4df25c5b8eee1ec5ff7b
Anukul <anukul@github.com>
2023-07-18 18:25:00 +0545

    added controflow.py
    added all 3 control flow statement

--- controlflow.py
+++ controlflow.py
@@ -1,11 +1,27 @@
-#if statement
-input1=int(input("Enter a first num: "))
-input2=int(input("Enter a second num "))
-if input1>input2:
-    print("First number is greater than Second Number")
-elif input1==input2:
-    print("Numbers Are equal")
-else:
-    print("Second number is greater than First number")
+#Global Inputs
+variable_a=int(input("Enter a first variable: "))
+#variable_b=int(input("Enter a Second Variable: "))
+# 1. if statement
+print("# if Statement")
+if variable_a>0:
+    print("A is Positive Integer")
 
-#3:23:22
+# 2.if -else statement
+print("# if - else statement")
+if variable_a>0:
+    print("A is positive Integer")
+else:
+    print("A is negative Integer")
+
+# 3. Elif statement
+print("#elif statement")
+if variable_a>0:
+    print("A is Positive Integer")
+elif variable_a<0:
+    print("A is Negative Integer")
+elif variable_a==0:
+    print("The Integer is Zero")
+
+# 
+
+#3:23:22  
