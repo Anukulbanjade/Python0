3d9ae385f9a8a9f7be6f1e0bc8d04b7dbf006197
Anukul <anukul@github.com>
2023-07-19 12:14:01 +0545

    while loops

--- loops.py
+++ loops.py
@@ -1,5 +1,18 @@
 #Loops in python
 # 1.while Loop
-print("1.while Loop")
+print("1. While Loops")
+n=int(input("Enter The number: "))
+i=0
+while i<n:
+    print(i)
+    i+=1
+print("Finished Iteration") 
 
+#Example of While Loops
+Input1=int(input("Enter the number you want to perform multiplication: "))
+print(f"The multiplication table of {Input1}  is ")
+j=1
+while j<=10:
+    print(Input1*j)
+    j+=1
 
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-#check grade of student from Marks
-marks=float(input("Enter The Marks: "))
-if marks>=90:
-    print("Congratulations A+ Grade")
-elif marks>=80 and marks<90:
-    print("Congratulation A Grade")
-elif marks>=70 and marks<80:
-    print("Great B+ Grade")
-elif (marks>=60 and marks<70):
-    print("Good, B grade")
-elif(marks>=50 and marks<60):
-    print("C+ grade, Try to improve")
-elif(marks>=40 and marks<50):
-    print("C grade, Try to improve")
-else:
-    print("Sorry You aren't qualified")    
+Input1=int(input("Enter the number you want to perform multiplication:"))
+print(f"The multiplication table of {Input1}  is ")
+j=1
+while j<=10:
+    print(Input1*j)
+    j+=1
+
