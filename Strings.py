f7469d877859e2ecf8b0f8f0174aaf9ca41e2664
Anukul <anukul@github.com>
2023-07-25 23:45:22 +0545

    repo update

--- Strings.py
+++ Strings.py
@@ -2,3 +2,33 @@
 s="python os a best language for data science "
 t="in this course we are going to learn python"
 print(s+'and '+t)
+print(len(s))
+print((s.strip()))
+
+
+#styles of printing
+price=120
+s="the price of this book"
+print(s+" is ",price)
+print(s+" is "+str(price))
+print(f"{s} is {price}")
+
+
+#multiline strings
+multiline="""Hello it's me anukul i am learning
+git and python simultaneously.
+now i am working with strings"""
+print(multiline)
+
+#indexing and slicing the strings
+
+#Syntax _ s[start:end,stepsize]
+print(multiline[7])
+
+print(multiline[2:10])
+print(multiline[-1])
+print(multiline[-8:-1])
+abc=multiline[1:15]
+print(abc)
+print(abc[::-1]) #reversing the string
+#8:05:24
