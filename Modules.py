c7414ffbedf46d38d0d8873b17e322d3d8713e69
Anukul <anukul@github.com>
2023-07-25 23:12:10 +0545

    importing local modules

--- Modules.py
+++ Modules.py
@@ -1,17 +1,6 @@
-def sorting_acending(*L):
-    L=[]
-    for j in range(len(*L)):
-        m=L[j]
-        idx=j
-        c=j
-        for i in range(j,len(L)):
-            if L[i]<m:
-                m=L[i]
-                idx=c
-            c+=1
-        tmp=L[j]
-        L[j]=m
-        L[idx]=tmp
-    print(L)
+import sys
+sys.path.append('D:\Projects\Python')
+import modules101 as mp
 
-sorting_acending(5,6,3,2,14,6)
+print(mp.sum(2,4,6))
+print(mp.multiplication(4,6,9))
